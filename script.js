147eba1dd3cbb68efb9291e23c037923f0b6825e
Abdifatah <abdifatah.101@outlook.com>
2021-11-01 19:55:05 +0000

    Sorted questions for script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
+const startContainer = document.getElementById("start");
+const quiz = document.getElementById("quiz");
+const question = document.getElementById("question");
+const counter = document.getElementById("counter");
+const choice1 = document.getElementById("first");
+const choice2 = document.getElementById("second");
+const choice3 = document.getElementById("third");
+const choice4 = document.getElementById("fourth");
+const progress = document.getElementById("ScoreBar");
+const scoreCon = document.getElementById("scoreContainer");
 
-const question = document.querySelector('#question');
-const choices = Array.from(document.querySelectorAll('.choice-text'));
-const progressText = document.querySelector('#progressText');
-const scoreText = document.querySelector('#score');
-const progressBarFull = document.querySelector('#progressBarFull');
 
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
-
+const lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+const questionTime = 10; // 10s
+let Timer;
+let score = 0;
     let questions = [
         {
             questions:'What does HTML stand for?',
@@ -54,66 +59,3 @@ let availableQuestions = []
     },
     ]
 
-const SCORE_POINTS = 100
-const MAX_QUESTIONS = 5
-
-startGame = () => {
- questionCounter = 0
- score = 0
- availableQuestions = [...questions]
- getNewQuestion()
-}
-
-getNewQuestion = () => {
- if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
- localStorage.setItem('mostRecentScore', score)
-
- return window.location.assign('/end.html')
-    }
-
- questionCounter++
- progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
- progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
-
- const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
- currentQuestion = availableQuestions[questionsIndex]
- question.innerText = currentQuestion.question
-
- choices.forEach(choice => {
- const number = choice.dataset['number']
- choice.innerText = currentQuestion['choice' + number]
-    })
- availableQuestions.splice(questionsIndex, 1)
-
- acceptingAnswers = true
-}
-
-choices.forEach(choice => {
- choice.addEventListener('click', e=> {
- if(!acceptingAnswers) return
-
- acceptingAnswers = false
- const selectedChoice = e.target
- const selectedAnswer = selectedChoice.dataset['number']
-
- let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :'incorrect'
-
- if(classToApply === 'correct') {
- incrementScore(SCORE_POINTS)
-        }
-
- selectedChoice.parentElement.classList.add(classToApply)
-
- setTimeout(() => {
- selectedChoice.parentElement.classList.remove(classToApply)
- getNewQuestion()
-        }, 1000)
-    })
-})
-
-incrementScore = num => {
- score += num
- scoreText.innerText = score
-}
-
-startGame()
